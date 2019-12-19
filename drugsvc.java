2a30d25b7fe3176a4b6d8377cb7ac98eb4add865
e <jun_jiehao@163.com>
2019-12-19 13:02:52 +0800

    create drugsvc

---
+++ drugsvc.java
@@ -0,0 +1,13 @@
+package com.fanshes;
+
+public class Teacher {
+    private int id;
+
+    private String name;
+
+    public Teacher() {
+    }
+    public  void test(){
+        System.out.println("helloWord");
+    }
+}
